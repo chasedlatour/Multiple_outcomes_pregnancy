560c553080e3c7388d1225a68fa8e89c81842fa0
chasedlatour <chasedlatour@gmail.com>
2024-01-26 16:07:15 -0500

    Wrote out analysis code for all scenarios

--- Analyses.R
+++ Analyses.R
@@ -36,3 +36,275 @@ select_save <- analyses %>%
 
 ## Save the RDS
 saveRDS(select_save, "analyses_scenario1.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 2
+#############################################
+
+## Load in the data
+data <- readRDS('scenario2.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario2.rds")
+
+
+
+#############################################
+## SCENARIO 3
+#############################################
+
+## Load in the data
+data <- readRDS('scenario3.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario3.rds")
+
+
+
+
+#############################################
+## SCENARIO 4
+#############################################
+
+## Load in the data
+data <- readRDS('scenario4.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario4.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 5
+#############################################
+
+## Load in the data
+data <- readRDS('scenario5.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario5.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 6
+#############################################
+
+## Load in the data
+data <- readRDS('scenario6.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario6.rds")
+
+
+
+
+#############################################
+## SCENARIO 7
+#############################################
+
+## Load in the data
+data <- readRDS('scenario7.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario7.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 8
+#############################################
+
+## Load in the data
+data <- readRDS('scenario8.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario8.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 9
+#############################################
+
+## Load in the data
+data <- readRDS('scenario9.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario9.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 10
+#############################################
+
+## Load in the data
+data <- readRDS('scenario10.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario10.rds")
+
+
+
+
+
+#############################################
+## SCENARIO 11
+#############################################
+
+## Load in the data
+data <- readRDS('scenario6.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario11.rds")
+
+
+
+
+
+
+#############################################
+## SCENARIO 12
+#############################################
+
+## Load in the data
+data <- readRDS('scenario6.rds')
+
+# Clean the data for analyses
+trial <- trial_cohort(data)
+
+## Analyze the cleaned data
+analyses <- clean_analyze(trial)
+
+## Select the information of-interest
+select_save <- analyses %>% 
+  select(-c(data))
+
+## Save the RDS
+saveRDS(select_save, "analyses_scenario12.rds")
+
+
+
+
+
+
+
+
+
+
+
